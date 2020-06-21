1c38061bb939e6e73339d718edab9f0acd52503f
Nicke Manarin <nicke@outlook.com.br>
2020-06-20 21:57:13 -0300

    What is love?

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   //publicPath: process.env.NODE_ENV === 'production' ? '/ScreenToGif-Website-Alpha/' : '/', //Erase later when using custom domain.
+  publicPath: '/',
   lintOnSave: false,
 
   pluginOptions: {
