d644a560f1d79fa9ad54e59b5c59bff040ca33aa
Nicke Manarin <nicke@outlook.com.br>
2020-09-08 10:24:50 -0300

    Translation update and small tweaks.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="app">
-        <Navigation />
-        <router-view />
-        <Footer />
+        <Navigation/>
+        <router-view/>
+        <Footer/>
     </div>
 </template>
 
--- src/components/TweetList.vue
+++ src/components/TweetList.vue
@@ -1,40 +1,40 @@
 <template>
     <div class="columns is-multiline is-centered">
         <div class="column is-one-third-widescreen is-half-desktop is-half-tablet is-inline-block">
-            <Tweet widget-class="twitter-card" id="1262326855119417345" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }">Loading Tweet</Tweet>
+            <Tweet widget-class="twitter-card" id="1262326855119417345" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }"/>
         </div>
 
         <div class="column is-one-third-widescreen is-half-desktop is-half-tablet is-inline-block">
-            <Tweet widget-class="twitter-card" id="1261313183597367298" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }">Loading Tweet</Tweet>
+            <Tweet widget-class="twitter-card" id="1261313183597367298" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }"/>
         </div>
 
         <div class="column is-one-third-widescreen is-half-desktop is-half-tablet is-inline-block">
-            <Tweet widget-class="twitter-card" id="1260470890950799362" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }">Loading Tweet</Tweet>
+            <Tweet widget-class="twitter-card" id="1260470890950799362" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }"/>
         </div>
 
         <div class="column is-one-third-widescreen is-half-desktop is-half-tablet is-inline-block">
-            <Tweet widget-class="twitter-card" id="1225829874453094401" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }">Loading Tweet</Tweet>
+            <Tweet widget-class="twitter-card" id="1225829874453094401" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }"/>
         </div>
 
         <div class="column is-one-third-widescreen is-half-desktop is-half-tablet is-inline-block">
-            <Tweet widget-class="twitter-card" id="1224858930901409809" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }">Loading Tweet</Tweet>
+            <Tweet widget-class="twitter-card" id="1224858930901409809" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }"/>
         </div>
 
         <div class="column is-one-third-widescreen is-half-desktop is-half-tablet is-inline-block">
-            <Tweet widget-class="twitter-card" id="1223660258264068096" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }">Loading Tweet</Tweet>
+            <Tweet widget-class="twitter-card" id="1223660258264068096" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }"/>
         </div>
 
 
         <div v-if="showAll" class="column is-one-third-widescreen is-half-desktop is-half-tablet is-inline-block">
-            <Tweet widget-class="twitter-card" id="1266162907710013441" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }">Loading Tweet</Tweet>
+            <Tweet widget-class="twitter-card" id="1266162907710013441" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }"/>
         </div>
 
         <div v-if="showAll" class="column is-one-third-widescreen is-half-desktop is-half-tablet is-inline-block">
-            <Tweet widget-class="twitter-card" id="1266348932331954176" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }">Loading Tweet</Tweet>
+            <Tweet widget-class="twitter-card" id="1266348932331954176" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }"/>
         </div>
 
         <div v-if="showAll" class="column is-one-third-widescreen is-half-desktop is-half-tablet is-inline-block">
-            <Tweet widget-class="twitter-card" id="1268451667009306626" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }">Loading Tweet</Tweet>
+            <Tweet widget-class="twitter-card" id="1268451667009306626" :options="{ cards: 'hidden', conversation: 'none', dnt: 'true' }"/>
         </div>
     </div>
 </template>
